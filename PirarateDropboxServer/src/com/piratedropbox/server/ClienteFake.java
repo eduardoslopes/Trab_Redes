9700d156b5ad57e8cc69d88d53ad035091bc5126
eduardoslopes <eduardo.lopes.es@gmail.com>
2016-01-08 13:37:16 -0300

    Testes funcionando

--- PirarateDropboxServer/src/com/piratedropbox/server/ClienteFake.java
+++ PirarateDropboxServer/src/com/piratedropbox/server/ClienteFake.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import com.piratedropbox.server.model.Arquivo;
 import com.piratedropbox.server.model.Mensagem;
 import com.piratedropbox.server.model.Pasta;
 import com.piratedropbox.server.model.TAG;
+import com.piratedropbox.server.model.Usuario;
 import com.piratedropbox.server.view.Mensageiro;
 
 public class ClienteFake {
@@ -15,8 +17,9 @@ public class ClienteFake {
 		try {
 			Socket conexao = new Socket("localhost", 8888);
 			//System.out.println("Olá "+apelido + " seja bem vindo!");
-			
-			Mensageiro sender = new Mensageiro(conexao, new Mensagem(TAG.CREATEP, new Pasta("Mii", 2) ));	
+			byte[] b = null;
+			Mensagem m = new Mensagem(TAG.INSERTAP, new Arquivo("foto.png", b), 4) ;
+			Mensageiro sender = new Mensageiro(conexao, m);	
 			sender.start();
 			
 		} catch (UnknownHostException e) {
--- PirarateDropboxServer/src/com/piratedropbox/server/model/Usuario.java
+++ PirarateDropboxServer/src/com/piratedropbox/server/model/Usuario.java
@@ -6,15 +6,20 @@ public class Usuario {
 	private String sobrenome;
 	private String senha;
 	private int idPastaRaiz;
-	
-	public Usuario() {}
-	
+		
 	public Usuario(String username, String senha, String nome, String sobrenome, int idPastaRaiz){
 		this.username = username;
 		this.senha = senha;
 		this.nome = nome;
 		this.sobrenome = sobrenome;
 		this.idPastaRaiz = idPastaRaiz;
+	}
+	
+	public Usuario(String username, String senha, String nome, String sobrenome){
+		this.username = username;
+		this.senha = senha;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
 	}
 	
 	public String getUsername() {
